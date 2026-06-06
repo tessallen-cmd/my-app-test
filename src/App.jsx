ae35fbf00db7a9f4f1528c0580b0a29a373fb068
tessallen-cmd <tessstablet@gmail.com>
2026-06-06 13:24:13 +0100

    add link to Github

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,19 @@ import './App.css'
 
  export default function App() {
 
-  return  <div className="App"> Weather App</div>;
+  return (
+     <div className="App">
+       Weather App
+       <p>
+
+
+        <a
+          href="https://github.com/tessallen-cmd/my-app-test"
+          target="_blank"
+        ></a>
+       </p> 
+       </div>
+  );
   }
 
  
